a32db513116466736130617d0b8dde81835259b5
jackie <jm20wd@gmail.com>
2024-08-12 17:56:39 +0100

    Change get route handler to fetch from database

--- index.js
+++ index.js
@@ -41,14 +41,17 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
+    /*const id = request.params.id
     const note = notes.find(note => note.id === id)
     
     if (note) {
         response.json(note)
     } else {
         response.status(404).end()
-    }
+    }*/
+    Note.findById(request.params.id).then(note => {
+        response.json(note)
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
