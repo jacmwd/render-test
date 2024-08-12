aafa805cdecdccbb7a2909cc8a619098807d8ab9
jackie <jm20wd@gmail.com>
2024-08-12 16:56:00 +0100

    Add module with db configuration

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const Note = require('./models/note')
 
+
+//const password = process.argv[2]
 
 app.use(express.json())
 app.use(cors())
@@ -30,7 +34,10 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    //response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
@@ -76,6 +83,6 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`server is running on ${PORT}`)
