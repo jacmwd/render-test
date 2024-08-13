7d6fcf64d14405c727cc2f3dba0d6ffb2fe51054
jackie <jm20wd@gmail.com>
2024-08-13 22:17:18 +0100

    Add put route handler to update entries in database

--- index.js
+++ index.js
@@ -102,6 +102,22 @@ app.post('/api/notes/', (request, response)=> {
     })
 })
 
+app.put('/api/notes/:id', (request, response)=> {
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important: body.important 
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+
+})
+
 //middleware catching requests made to unknown routes
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
