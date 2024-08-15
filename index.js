464bb35e4ba47cbc6e3295c440103ec54a3995be
jackie <jm20wd@gmail.com>
2024-08-15 23:25:47 +0100

    Add request logger middleware

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const cors = require('cors')
 const app = express()
 const Note = require('./models/note')
 
+const requestLogger = (request, response, next) => {
+    console.log('Method: ', request.method)
+    console.log('Path: ', request.path)
+    console.log('Body: ', request.body)
+    console.log('---')
+    next()
+}
+
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
+app.use(requestLogger)
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
